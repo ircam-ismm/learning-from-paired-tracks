1da08aec7634967095a8447286ff709808ed3ce4
BalthazarBujard <115577878+BalthazarBujard@users.noreply.github.com>
2025-07-23 10:35:13 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -324,6 +324,10 @@ def build_coupling_ds(roots : List[List[Path]],
         sampler=DistributedSampler(ds)
         shuffle=False
     
+    if batch_size == None:
+        batch_size = len(ds) #batch size is all the track chunks
+    
+    
     loader = DataLoader(ds, batch_size, shuffle=shuffle,sampler=sampler, #with distributed shuffle = false
                         collate_fn=collate_fn,num_workers=2,pin_memory=True)
 
